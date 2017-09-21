d8f900f16afd58b8459a9167ac843b61c8d00c63
ashishjain28 <ashishjain.2894@gmail.com>
2017-09-21 23:43:23 +0530

    static and media settings added, Profile model defined

--- follow/follow/settings.py
+++ follow/follow/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'follow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'follow',
+        'USERNAME': 'ashish',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
@@ -121,3 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+    ]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+# Media files (uploaded by user)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
+# INTERNAL_IPS = '127.0.0.0'
--- follow/social/models.py
+++ follow/social/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+def upload_location(instance, filename):
+    return 'dp/' + instance + '/' + filename
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key=True,
+                                related_name='Profile',
+                                on_delete=models.CASCADE
+                                )
+    mobile_number = models.PositiveIntegerField(max_length=10,
+                                                blank=False
+                                                )
+    is_mobile_visible = models.BooleanField(default=False)
+    is_email_visible = models.BooleanField(default=False)
+    picture = models.ImageField(blank=True,
+                                upload_to=upload_location)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        db_table = 'user_profile',
+        verbose_name = 'UserProfile',
+        verbose_name_plural = 'UserProfiles'
