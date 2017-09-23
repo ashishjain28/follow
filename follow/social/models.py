7ccaaa38403b3ddb5e95ee671e51b39bc968d9c6
ashishjain28 <ashishjain.2894@gmail.com>
2017-09-23 12:11:59 +0530

    import corrected in admin, ManyToMany added in Profile, OneToOne changed to ForeignKey in Post

--- follow/social/models.py
+++ follow/social/models.py
@@ -9,17 +9,16 @@ def upload_location(instance, filename):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, primary_key=True,
+    user = models.OneToOneField('User', primary_key=True,
                                 related_name='Profile',
                                 on_delete=models.CASCADE
                                 )
-    mobile_number = models.PositiveIntegerField(max_length=10,
-                                                blank=False
-                                                )
+    mobile_number = models.PositiveIntegerField(blank=False)
     is_mobile_visible = models.BooleanField(default=False)
     is_email_visible = models.BooleanField(default=False)
     image = models.ImageField(blank=True,
                               upload_to=upload_location)
+    friends = models.ManyToManyField("self", blank=True)
 
     def __str__(self):
         return self.user.username
@@ -38,10 +37,9 @@ class Post(models.Model):
     content = models.TextField(max_length=200, blank=False)
     post_image = models.ImageField(blank=True,
                                    upload_to=post_upload_location)
-    user = models.OneToOneField(User)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     post_date = models.DateTimeField(auto_now_add=True)
     likes_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.content + 'by' + self.user.username
-
